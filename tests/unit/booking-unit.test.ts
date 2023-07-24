ffa8401b6e5ba3cc7944a0385d30ef9cecd079a6
Lorena <blorena1321@gmail.com>
2023-07-24 13:02:12 -0300

    coverage page

---
+++ tests/unit/booking-unit.test.ts
@@ -0,0 +1,19 @@
+describe("Booking service unit tests",() => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe("get booking tests", () => {
+        it("", async ()=> {
+
+        });
+
+        it("", async ()=> {
+
+        });
+
+        it("", async ()=> {
+
+        });
+    })
+})
